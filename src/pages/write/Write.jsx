7b4249c0b42feaf3ae483799b6782b9e5f0cc859
Abdu_ <abderraouf.dev@gmail.com>
2022-05-31 23:07:07 +0100

    added the saved clean data functionality

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React ,{useRef, useCallback} from 'react'
 import "./write.css"
 import {createReactEditorJS} from 'react-editor-js'
 import {EDITOR_JS_TOOLS} from './tools'
 const EditorJsWrite = createReactEditorJS()
 export default function Write() {
     
+  const editorCore = React.useRef(null)
+
+const handleInitialize = React.useCallback((instance) => {
+  editorCore.current = instance
+  console.log('sdfgsdfgsdfg')
+}, [])
+
+const handleSave = React.useCallback(async () => {
+  const savedData = await editorCore.current.save();
+  //console.log(savedData.blocks);
+}, [])
+
   return (
     <div className="write">
 
@@ -12,17 +24,20 @@ export default function Write() {
             <div className="left">Create Pose</div>
             <div className="right">
               <div className="header-ops" id='edit'>Edit</div>
-              <div className="header-ops" id='preview'>Preview</div>
+              <div onClick={handleSave} className="header-ops" id='preview'>Preview</div>
             </div>
           </div>
-          
+
         <div className="write-container">
               <form action="#">
               <input type="text" name="title" id="title" className="title" placeholder='New post title here...'/>
               <input type="text" name="tags" id="tags" className="tags" placeholder='Add up to 4 tags...'/>
-              <EditorJsWrite  tools={EDITOR_JS_TOOLS} />
+              <EditorJsWrite
+              tools={EDITOR_JS_TOOLS}
+              onInitialize={handleInitialize}
+              />
               <div className="post-btns">
-                <button className='publish-btn btn' type="submit">Publish</button>
+                <button  className='publish-btn btn' type="submit">Publish</button>
                 <button className='draft-btn btn' type="submit">Save Draft</button>
               </div>
               </form>
