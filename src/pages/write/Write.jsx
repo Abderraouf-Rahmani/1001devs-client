9953cf069435bc4c28b2354cbef07a02cad11ccb
Abderraouf-Rahmani <95105187+Abderraouf-Rahmani@users.noreply.github.com>
2022-06-01 10:15:55 +0100

    add the marker and embed plugins, as well as the placeholder for the editor input

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,48 +1,66 @@
-import React ,{useRef, useCallback} from 'react'
-import "./write.css"
-import {createReactEditorJS} from 'react-editor-js'
-import {EDITOR_JS_TOOLS} from './tools'
-const EditorJsWrite = createReactEditorJS()
+import React, { useRef, useCallback } from "react";
+import "./write.css";
+import { createReactEditorJS } from "react-editor-js";
+import { EDITOR_JS_TOOLS } from "./tools";
+const EditorJsWrite = createReactEditorJS();
 export default function Write() {
-    
-  const editorCore = React.useRef(null)
+  const editorCore = React.useRef(null);
 
-const handleInitialize = React.useCallback((instance) => {
-  editorCore.current = instance
-  console.log('sdfgsdfgsdfg')
-}, [])
+  const handleInitialize = React.useCallback((instance) => {
+    editorCore.current = instance;
+    console.log("sdfgsdfgsdfg");
+  }, []);
 
-const handleSave = React.useCallback(async () => {
-  const savedData = await editorCore.current.save();
-  //console.log(savedData.blocks);
-}, [])
+  const handleSave = React.useCallback(async () => {
+    const savedData = await editorCore.current.save();
+    //console.log(savedData.blocks);
+  }, []);
 
   return (
     <div className="write">
-
-          <div className="write-container-header">
-            <div className="left">Create Pose</div>
-            <div className="right">
-              <div className="header-ops" id='edit'>Edit</div>
-              <div onClick={handleSave} className="header-ops" id='preview'>Preview</div>
-            </div>
+      <div className="write-container-header">
+        <div className="left">Create Pose</div>
+        <div className="right">
+          <div className="header-ops" id="edit">
+            Edit
           </div>
-
-        <div className="write-container">
-              <form action="#">
-              <input type="text" name="title" id="title" className="title" placeholder='New post title here...'/>
-              <input type="text" name="tags" id="tags" className="tags" placeholder='Add up to 4 tags...'/>
-              <EditorJsWrite
-              tools={EDITOR_JS_TOOLS}
-              onInitialize={handleInitialize}
-              />
-              <div className="post-btns">
-                <button  className='publish-btn btn' type="submit">Publish</button>
-                <button className='draft-btn btn' type="submit">Save Draft</button>
-              </div>
-              </form>
-
+          <div onClick={handleSave} className="header-ops" id="preview">
+            Preview
+          </div>
         </div>
+      </div>
+
+      <div className="write-container">
+        <form action="#">
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="title"
+            placeholder="New post title here..."
+          />
+          <input
+            type="text"
+            name="tags"
+            id="tags"
+            className="tags"
+            placeholder="Add up to 4 tags..."
+          />
+          <EditorJsWrite
+            placeholder={"Write your post content here..."}
+            tools={EDITOR_JS_TOOLS}
+            onInitialize={handleInitialize}
+          />
+          <div className="post-btns">
+            <button className="publish-btn btn" type="submit">
+              Publish
+            </button>
+            <button className="draft-btn btn" type="submit">
+              Save Draft
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
