ee3f9710f42127470f470e23811ef56428775717
Abderraouf-Rahmani <95105187+Abderraouf-Rahmani@users.noreply.github.com>
2022-06-02 09:08:38 +0100

    changed the navbar layout add the buttons when the user is ligged in

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,37 +1,73 @@
-
-import "./navbar.css"
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import SearchIcon from '@mui/icons-material/Search';
-
+import "./navbar.css";
+import image from "../../img/logo.png";
+import profilePic from "../../img/Troll-face.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LunchDiningIcon from "@mui/icons-material/LunchDining";
 
 export default function Navbar() {
- 
+  let isUser = true;
+  let isPhoneView = false;
+  const handleRes = () => {
+    if (window.innerWidth < 730) {
+      isPhoneView = true;
+      console.log("ppp");
+    } else {
+      isPhoneView = false;
+    }
+  };
+  window.onresize = handleRes;
   return (
-    <nav className='navbar'>
-       <div className="container">
-           <div className="left">
-           <TwitterIcon className='nav-icon' />
-           <LinkedInIcon className='nav-icon' />
-           <YouTubeIcon className='nav-icon' />
-           </div>
-           <div className="center">
-               
-                   <div className="nav-item">Home</div>
-                   <div className="nav-item">Brows</div>
-                   <div className="nav-item">About</div>
-                   <div className="nav-item">contact</div>
-               
-           </div>
-           <div className="right">
-                    
-                   <div className="nav-item">Login</div>
-                   <div className="nav-item">Join us!</div>
-                   <SearchIcon  className='nav-icon' />
-               
-           </div>
-       </div>
+    <nav className="navbar">
+      <div className="container">
+        <div className="left">
+          <div className="nav-icon-container">
+            <LunchDiningIcon className="nav-icon" />
+          </div>
+          <div className="nav-icon">
+            <img className="logo" src={image} alt="logo" />
+          </div>
+          <div className="nav-search-container">
+            <form action="#" className="nav-search-form">
+              <input
+                type="text"
+                name="search"
+                id="search"
+                className="search"
+                placeholder="Search..."
+              />
+              <button type="submit" className="search-submit-btn">
+                <SearchIcon className="nav-icon" />
+              </button>
+            </form>
+          </div>
+        </div>
+
+        <div className="right">
+          {/* IF THE USER IS LOGGED IN */}
+
+          {isUser ? (
+            <>
+              <div className="primary-btn">Create Post</div>
+              <div className="nav-icon-container">
+                <NotificationsNoneIcon className="nav-icon" />
+              </div>
+              <div className="profile-icon">
+                <img
+                  src={profilePic}
+                  alt="profile pic"
+                  className="profil-pic"
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="nav-item">Log in</div>
+              <div className="primary-btn">Create account</div>
+            </>
+          )}
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
