d502aa854dcc917910d065993ef2c43541cf8c52
Abderraouf-Rahmani <95105187+Abderraouf-Rahmani@users.noreply.github.com>
2022-06-03 12:16:49 +0100

    Improved Home page styling

--- src/components/postcard/Postcard.jsx
+++ src/components/postcard/Postcard.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 import "./postcard.css";
 export default function Postcard() {
   return (
     <div className="post-card">
+      <div className="card-header">
         <div className="card-title">CSS</div>
         <div className="card-date">Aug, 31</div>
-        <div className="card-desc-container">
-          <div className="card-desc">You don't know FlexBox</div>  
-        </div>
+      </div>
+      <div className="card-desc-container">
+        <div className="card-desc">You don't know FlexBox</div>
+      </div>
     </div>
-  )
+  );
 }
