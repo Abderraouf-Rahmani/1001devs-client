02e1e8990e872a8695425d6bcfd88f425a8aa888
Abderraouf-Rahmani <95105187+Abderraouf-Rahmani@users.noreply.github.com>
2022-06-03 10:52:18 +0100

    made the home page responsive :) It was a pain in the A**

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import Navbar from "./components/navbar/Navbar.jsx"
-import Home from "./pages/home/Home.jsx"
+import Navbar from "./components/navbar/Navbar.jsx";
+import Home from "./pages/home/Home.jsx";
 //import Post from "./pages/post/Post.jsx"
 //import Login from "./pages/login/Login.jsx";
 //import Register from "./pages/register/Register.jsx";
-import Footer from "./components/footer/Footer.jsx"
+import Footer from "./components/footer/Footer.jsx";
 //import Write from "./pages/write/Write.jsx";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home /> 
-      <Footer /> 
-      
+      <Home />
+      <Footer />
     </div>
   );
 }
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import "./posts.css"
-import Postcard from "../postcard/Postcard"
+import React from "react";
+import "./posts.css";
+import Postcard from "../postcard/Postcard";
 
 export default function Posts() {
   return (
     <div className="posts">
-     <div className="container">
-      <Postcard />
-      <Postcard />
-      <Postcard />
-      <Postcard />
-      <Postcard />
-      <Postcard />
-      <Postcard />
-      <Postcard />
-      
-
-     </div>
+      <div className="container">
+        <div className="posts-filters">
+          <div className="post-filter chosen">Relevant</div>
+          <div className="post-filter">Latest</div>
+          <div className="post-filter">Top</div>
+        </div>
+        <div className="post-cards-container">
+          <Postcard />
+          <Postcard />
+          <Postcard />
+          <Postcard />
+          <Postcard />
+          <Postcard />
+          <Postcard />
+          <Postcard />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
