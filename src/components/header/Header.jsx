7ac06c5d4ce1b75eb48c7ec32cb4fa017280e9b0
Abderraouf-Rahmani <95105187+Abderraouf-Rahmani@users.noreply.github.com>
2022-06-03 09:46:53 +0100

    improved the header styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 import "./header.css";
-
 
 export default function Header() {
   return (
-      <div className="header">
-        <div className="container">
-            <div className="header-content">
-                <div className="header-title">Welcome to 1001 Devs</div>
-                <div className="header-desc">Join us and let's learn togather :)</div>
-                <div className="btns">
-                <button className="header-join-btn">Join US</button>
-                <button className="header-login-btn">Login</button>
-
-                </div>
-            </div>
+    <div className="header">
+      <div className="container">
+        <div className="header-content">
+          <div className="header-title">
+            Welcome to <span>1001DEVs</span>
+          </div>
+          <div className="header-desc">Join us and let's learn togather :)</div>
+          <div className="btns">
+            <button className="header-join-btn">Join US</button>
+            <button className="header-login-btn">Login</button>
+          </div>
         </div>
       </div>
-  )
+    </div>
+  );
 }
