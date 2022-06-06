7b45bc5d201745b9eb3e189e6ce4a74e76bd3064
Abderraouf-Rahmani <95105187+Abderraouf-Rahmani@users.noreply.github.com>
2022-06-06 10:37:05 +0100

    styled the profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,31 +1,44 @@
-import React from 'react'
-import "./profile.css"
-import SearchCard from '../../components/searchcard/SearchCard';
-import image from "../../img/logo.png";
+import React from "react";
+import "./profile.css";
+import SearchCard from "../../components/searchcard/SearchCard";
+import image from "../../img/Troll-face.png";
+import CakeIcon from "@mui/icons-material/Cake";
 
 export default function Profile() {
   return (
-        <div className="profile">
-            <div className="cover"></div>
-            <div className="profile-container">
-                <div className="profile-header">
-                    <div className="profile-pic-container">
-                        <img src={image} alt="profile pic" className="profile-pic" />
-                    </div>
-                    <h3 className="name">Abderraouf Rahmani</h3>
-                    <div className="about">Hi, I'm Abderraouf, you can call me Abdu, I love Coding and problem solving in genera</div>
-                    <div className="joined-date">Joined on Aug 31, 2021</div>
-                </div>
-                <div className="profile-infos">
-                    <div className="skills">
-                        <h3 className="skills-title">Skills/Languages</h3>
-                        <div className="the-skills">beside the obvious (HTML & CSS), JavaScript, React, NodeJs, ExpressJs, MongoDB, MySQL.
-                    </div>
-                </div>
-                    <div className="post-hist"><SearchCard /></div>
-                </div>
-            </div>
-
+    <div className="profile">
+      <div className="cover"></div>
+      <div className="profile-container">
+        <div className="profile-header">
+          <div className="profile-pic-container">
+            <img src={image} alt="profile pic" className="profile-pic" />
+          </div>
+          <h3 className="name">Abderraouf Rahmani</h3>
+          <div className="about">
+            Hi, I'm Abderraouf, you can call me Abdu, I love Coding and problem
+            solving in genera
+          </div>
+          <div className="joined-date">
+            <CakeIcon /> Joined on Aug 31, 2021
+          </div>
         </div>
-    )
+        <div className="profile-infos">
+          <div className="skills">
+            <h3 className="skills-title">Skills/Languages</h3>
+            <div className="the-skills">
+              <p>
+                beside the obvious (HTML & CSS), JavaScript, React, NodeJs,
+                ExpressJs, MongoDB, MySQL.
+              </p>
+            </div>
+          </div>
+          <div className="post-hist">
+            <SearchCard />
+            <SearchCard />
+            <SearchCard />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
