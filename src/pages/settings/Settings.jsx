333b2ee682f90375dac964a46e8c1b3799230494
Abderraouf-Rahmani <95105187+Abderraouf-Rahmani@users.noreply.github.com>
2022-06-06 15:55:58 +0100

    added settings page

---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./settings.css";
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="page-title">
+        <h1>Settings</h1>
+      </div>
+      <form className="setting-form" action="#">
+        <div className="user-settings">
+          <h2>User</h2>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="name"
+            placeholder="John Doe"
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            className="email"
+            placeholder="John.Doe@exmple.com"
+          />
+
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            className="username"
+            placeholder="JohnDoe"
+          />
+          <div className="profile-pic-settings">
+            <label htmlFor="pic">Profile image</label>
+            <input type="file" name="pic" id="pic" />
+          </div>
+        </div>
+        <div className="skills-settings">
+          <label htmlFor="email">Skills & Languages</label>
+          <p>
+            What tools and languages are you most experienced with? Are you
+            specialized or more of a generalist?
+          </p>
+          <textarea name="skills" id="skills" className="skills" />
+        </div>
+        <button type="submit" className="settings-btn">
+          Save profile informations
+        </button>
+      </form>
+    </div>
+  );
+}
