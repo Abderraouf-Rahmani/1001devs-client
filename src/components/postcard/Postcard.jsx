36e4fee5dee69d34422485aefd2c5e90c9bca775
Abderraouf-Rahmani <95105187+Abderraouf-Rahmani@users.noreply.github.com>
2022-06-06 16:25:07 +0100

    added the Link navigations

--- src/components/postcard/Postcard.jsx
+++ src/components/postcard/Postcard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./postcard.css";
 export default function Postcard() {
   return (
-    <div className="post-card">
-      <div className="card-header">
-        <div className="card-title">CSS</div>
-        <div className="card-date">Aug, 31</div>
+    <Link to="read/learn-flexbox" style={{ textDecoration: "none" }}>
+      <div className="post-card">
+        <div className="card-header">
+          <div className="card-title">CSS</div>
+          <div className="card-date">Aug, 31</div>
+        </div>
+        <div className="card-desc-container">
+          <div className="card-desc">You don't know FlexBox</div>
+        </div>
       </div>
-      <div className="card-desc-container">
-        <div className="card-desc">You don't know FlexBox</div>
-      </div>
-    </div>
+    </Link>
   );
 }
