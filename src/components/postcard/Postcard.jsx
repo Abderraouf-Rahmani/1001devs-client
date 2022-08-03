82322f37666282a84433378087cca4fddb75d3a3
Abdu_ <abderraouf.dev@gmail.com>
2022-08-04 00:24:18 +0100

    dates format done

--- src/components/postcard/Postcard.jsx
+++ src/components/postcard/Postcard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import formatDate from "../../util/toFormatedDate";
 import "./postcard.css";
 export default function Postcard({post}) {
  return (
@@ -11,7 +12,7 @@ export default function Postcard({post}) {
                 <span key={c} className="search-tag">#{c}</span>
               ))}
           </div>
-          <div className="card-date">{`${Notification}`}</div>
+          <div className="card-date">{`${formatDate(post.updatedAt)}`}</div>
         </div>
         <div className="card-desc-container">
           <div className="card-desc">{post.title}</div>
