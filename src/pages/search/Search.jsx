f8849abfe337fd4dc6aa29f10f6cf4fce5b9c323
ABDERRAOUF RAHMANI <95105187+Abderraouf-Rahmani@users.noreply.github.com>
2022-08-05 09:22:48 +0000

    typos -_-

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -43,7 +43,7 @@ export default function Search() {
             
         </div>
         <div className="search-container">
-          {isFetching && <img src="https://digitalsynopsis.com/wp-content/uploads/2016/06/loading-animations-preloader-gifs-ui-ux-effects-3.gif" alt="Loading" }
+          {isFetching && <img src="https://digitalsynopsis.com/wp-content/uploads/2016/06/loading-animations-preloader-gifs-ui-ux-effects-3.gif" alt="Loading" /> }
            {(posts.length === 0 && !isFetching) &&<> <div>No results match that query</div> <img src='https://cdni.iconscout.com/illustration/premium/thumb/folder-is-empty-4064360-3363921.png' /> </>}
           
             {!isFetching && (posts.map(p=>(
